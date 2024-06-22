8546eb02ceb70cf8a64eaa3610c294ca648a0337
ziadmyassin <ziady5060@gmail.com>
2024-06-22 11:37:26 +0300

    third commit

--- DB/connectionDB.js
+++ DB/connectionDB.js
@@ -1,9 +1,14 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("assignment-6", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+export const sequelize = new Sequelize(
+  "bvyox59w8qykwxs5pggj",
+  "u3gha6vunhrw0cyl",
+  "U1PHMQZVXtZ6M4f7aKNR",
+  {
+    host: "bvyox59w8qykwxs5pggj-mysql.services.clever-cloud.com",
+    dialect: "mysql",
+  }
+);
 
 export const connection = async () => {
   await sequelize
